df3ab6a417b6413fa54c231c20a1111db24a066b
Aritra Biswas <aritra.statistics@gmail.com>
2017-01-22 23:31:09 +0530

    easy way

--- freqency_distribution.R
+++ freqency_distribution.R
@@ -1,3 +1,10 @@
+
+###################__METHOD 0__###################################
+library(fdth)
+x<-c(0.23,0.27,0.30,0.25,0.27,0.26,0.40,0.40,0.51,0.58,0.61,0.80,0.90,1.02,0.92,1.01,0.76,0.90,1.14,1.11,1.38,1.52,0.96,1.16,1.52,1.05,1.51,1.36,0.91,1.22,1.54,1.35,1.76,2.00,1.01,1.69,1.51,2.00,1.45,1.38)
+freqdist<-fdt(x)
+freqdist
+
 # The weights of a diamond and other precious stones are usually measured in carats.  One carat is traditionally equal to 200 milligrams.  A random sample of the weights(in carats) of conflict-free loose diamonds is as follows:
 # 0.23	0.27	0.30	0.25	0.27	0.26	0.40	0.40
 # 0.51	0.58	0.61	0.80	0.90	1.02	0.92	1.01
@@ -8,6 +15,8 @@
 # b.) multiply each ovservation in the table by 200, to convert the weights into milligrams.  Construct a frequency distribution and a histogram for the new transformed data.
 # c.) compare the two histograms.  Are the shapes simlilar? Describe any differences.
 
+
+###################__METHOD 1__################################### 
 x<-c(0.23,0.27,0.30,0.25,0.27,0.26,0.40,0.40,0.51,0.58,0.61,0.80,0.90,1.02,0.92,1.01,0.76,0.90,1.14,1.11,1.38,1.52,0.96,1.16,1.52,1.05,1.51,1.36,0.91,1.22,1.54,1.35,1.76,2.00,1.01,1.69,1.51,2.00,1.45,1.38)
 range(x)
 bin1<-seq(min(x),max(x),by=.25)
